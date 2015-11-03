7a6be47a0ccddd0d0c87e7ce3fd9fff088e4e875
chiyongchun <chiyongchun@nhn.com>
2015-11-03 10:34:40 +0800

    web -> agent

--- dependencies.sh
+++ dependencies.sh
@@ -2,12 +2,10 @@
 # Dependencies
 
 go get -u github.com/lucas-chi/push-service
-go get -u github.com/lucas-chi/push-service/conf
 go get -u github.com/garyburd/redigo/redis
 go get -u code.google.com/p/go.net/websocket
 go get -u github.com/samuel/go-zookeeper
 go get -u code.google.com/p/log4go
-go get -u github.com/go-sql-driver/mysql
 
 #go get -u ./message/...
 #go get -u ./comet/...

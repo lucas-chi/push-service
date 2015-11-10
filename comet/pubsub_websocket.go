d790303aa43904fa40a01543bdf19c26b2d6a31e
chiyongchun <chiyongchun@nhn.com>
2015-11-10 10:24:34 +0800

    fixed websocket listen

--- comet/pubsub_websocket.go
+++ comet/pubsub_websocket.go
@@ -46,25 +46,30 @@ func StartWebsocket() error {
 }
 
 func websocketListen(bind string) {
-	httpServeMux := http.NewServeMux()
+	var (
+		listener     *net.TCPListener
+		addr         *net.TCPAddr
+		httpServeMux = http.NewServeMux()
+		err error
+	)
 	httpServeMux.Handle("/sub", websocket.Handler(SubscribeHandle))
-	if Conf.TCPKeepalive {
-		server := &http.Server{Handler: httpServeMux}
-		l, err := net.Listen("tcp", bind)
-		if err != nil {
-			log.Error("net.Listen(\"tcp\", \"%s\") error(%v)", bind, err)
-			panic(err)
-		}
-		if err := server.Serve(&KeepAliveListener{Listener: l}); err != nil {
+	
+	if addr, err = net.ResolveTCPAddr("tcp4", bind); err != nil {
+			log.Error("net.ResolveTCPAddr(\"tcp4\", \"%s\") error(%v)", bind, err)
+			return
+	}
+	if listener, err = net.ListenTCP("tcp4", addr); err != nil {
+		log.Error("net.ListenTCP(\"tcp4\", \"%s\") error(%v)", bind, err)
+		return
+	}
+	server := &http.Server{Handler: httpServeMux}
+	log.Debug("start websocket listen: \"%s\"", bind)
+	go func() {
+		if err = server.Serve(listener); err != nil {
 			log.Error("server.Serve(\"%s\") error(%v)", bind, err)
 			panic(err)
 		}
-	} else {
-		if err := http.ListenAndServe(bind, httpServeMux); err != nil {
-			log.Error("http.ListenAdServe(\"%s\") error(%v)", bind, err)
-			panic(err)
-		}
-	}
+	}()
 }
 
 // Subscriber Handle is the websocket handle for sub request.
